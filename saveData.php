c81b03bbca2e3c9fe279b66dde358e03b4e9ee1c
cadooy <rbnm.off@gmail.com>
2024-12-13 22:59:41 +0100

    Delete saveData.php

--- saveData.php
+++
@@ -1,9 +0,0 @@
-<?php
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $data = json_decode(file_get_contents('php://input'), true);
-    file_put_contents('data.json', json_encode($data, JSON_PRETTY_PRINT));
-    echo 'Data saved successfully!';
-} else {
-    echo 'Invalid request method.';
-}
-?>
